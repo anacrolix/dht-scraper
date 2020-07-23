022cd6f4cdcf639f9cafa5d4c32cbd4d6f192c02
Matt Joiner <anacrolix@gmail.com>
2020-07-23 14:08:01 +1000

    black

--- bencode.py
+++ bencode.py
@@ -1,31 +1,34 @@
-
 def bencode(value):
-	return {
-		dict: bencode_dict,
-		list: bencode_list,
-		int: bencode_int,
-		str: bencode_str,
-	}[type(value)](value)
+    return {
+        dict: bencode_dict,
+        list: bencode_list,
+        int: bencode_int,
+        str: bencode_str,
+    }[type(value)](value)
+
 
 def bencode_dict(value):
-	yield 'd'
-	for key, value in value.items():
-		yield from bencode(key)
-		yield from bencode(value)
-	yield 'e'
+    yield "d"
+    for key, value in value.items():
+        yield from bencode(key)
+        yield from bencode(value)
+    yield "e"
+
 
 def bencode_list(value):
-	yield 'l'
-	for item in list:
-		yield bencode(item)
-	yield 'e'
+    yield "l"
+    for item in list:
+        yield from bencode(item)
+    yield "e"
+
 
 def bencode_int(int):
-	yield 'i'
-	yield str(int)
-	yield 'e'
+    yield "i"
+    yield str(int)
+    yield "e"
+
 
 def bencode_str(_str):
-	yield str(len(_str))
-	yield ':'
-	yield _str
+    yield str(len(_str))
+    yield ":"
+    yield _str
--- test_main.py
+++ test_main.py
@@ -1,4 +1,12 @@
 from bencode import bencode
 
+
 def test_bencode_ping():
-	assert ''.join(bencode({"a":{"id":"abcdefghij0123456789"},"q":"ping","t":"aa", "y":"q",  })) == 'd1:ad2:id20:abcdefghij0123456789e1:q4:ping1:t2:aa1:y1:qe'
+    assert (
+        "".join(
+            bencode(
+                {"a": {"id": "abcdefghij0123456789"}, "q": "ping", "t": "aa", "y": "q",}
+            )
+        )
+        == "d1:ad2:id20:abcdefghij0123456789e1:q4:ping1:t2:aa1:y1:qe"
+    )
