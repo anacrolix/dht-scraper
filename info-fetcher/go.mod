8a079da1a1c4590c2d3a7ba68f8b6e54ab481a1a
Matt Joiner <anacrolix@gmail.com>
2020-08-27 11:56:03 +1000

    info-fetcher now fetches infos into the db

--- info-fetcher/go.mod
+++ info-fetcher/go.mod
@@ -5,6 +5,8 @@ go 1.14
 require (
 	crawshaw.io/sqlite v0.3.3-0.20200622053215-ae45c9066f6e
 	github.com/PuerkitoBio/goquery v1.5.1 // indirect
+	github.com/anacrolix/dht/v2 v2.6.1 // indirect
+	github.com/anacrolix/envpprof v1.1.0
 	github.com/anacrolix/torrent v1.15.1-0.20200811014312-e992bb78b9df
 	github.com/mattn/go-sqlite3 v2.0.2+incompatible
 	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
--- info-fetcher/main.go
+++ info-fetcher/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"sync"
 
 	"crawshaw.io/sqlite"
 	"crawshaw.io/sqlite/sqlitex"
+	"github.com/anacrolix/envpprof"
 	"github.com/anacrolix/torrent"
 )
 
@@ -18,7 +22,7 @@ func main() {
 }
 
 func getInfohashes() (ret []torrent.InfoHash, err error) {
-	conn, err := sqlite.OpenConn("herp.db")
+	conn, err := getDatabaseConn()
 	if err != nil {
 		return nil, fmt.Errorf("opening db: %w", err)
 	}
@@ -38,18 +42,45 @@ func getInfohashes() (ret []torrent.InfoHash, err error) {
 	return
 }
 
+func getDatabaseConn() (*sqlite.Conn, error) {
+	return sqlite.OpenConn("herp.db")
+}
+
 func mainErr() error {
+	defer envpprof.Stop()
 	cl, err := torrent.NewClient(nil)
 	if err != nil {
 		return err
 	}
 	defer cl.Close()
+	http.HandleFunc("/torrentClientStatus", func(w http.ResponseWriter, r *http.Request) {
+		cl.WriteStatus(w)
+	})
 	ihs, err := getInfohashes()
 	if err != nil {
 		return fmt.Errorf("getting infohashes: %w", err)
 	}
+	var wg sync.WaitGroup
 	for _, ih := range ihs {
-		fmt.Printf("%s\n", ih.HexString())
+		t, _ := cl.AddTorrentInfoHash(ih)
+		wg.Add(1)
+		go func(ih torrent.InfoHash) {
+			defer t.Drop()
+			<-t.GotInfo()
+			conn, err := getDatabaseConn()
+			if err != nil {
+				log.Printf("error getting database conn: %v", err)
+				return
+			}
+			defer conn.Close()
+			err = sqlitex.Exec(conn, "insert or ignore into info(infohash, bytes) values (?, ?)", nil, ih, []byte(t.Metainfo().InfoBytes))
+			if err != nil {
+				log.Printf("error inserting info into db: %v", err)
+				return
+			}
+			log.Printf("got info bytes for %v", ih)
+		}(ih)
 	}
+	wg.Wait()
 	return nil
 }
