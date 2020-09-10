ae33606ea1338e80d9fafab40cdea2a99fc1c940
Matt Joiner <anacrolix@gmail.com>
2020-09-10 14:39:31 +1000

    Improvements to info-fetcher

--- info-fetcher/go.mod
+++ info-fetcher/go.mod
@@ -7,7 +7,10 @@ require (
 	github.com/PuerkitoBio/goquery v1.5.1 // indirect
 	github.com/anacrolix/dht/v2 v2.6.1 // indirect
 	github.com/anacrolix/envpprof v1.1.0
+	github.com/anacrolix/log v0.7.1-0.20200604014615-c244de44fd2d
 	github.com/anacrolix/torrent v1.15.1-0.20200811014312-e992bb78b9df
 	github.com/mattn/go-sqlite3 v2.0.2+incompatible
+	github.com/rs/dnscache v0.0.0-20190621150935-06bb5526f76b // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
 	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
 )
--- info-fetcher/main.go
+++ info-fetcher/main.go
@@ -1,22 +1,25 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"sync"
+	"time"
 
 	"crawshaw.io/sqlite"
 	"crawshaw.io/sqlite/sqlitex"
 	"github.com/anacrolix/envpprof"
+	"github.com/anacrolix/log"
 	"github.com/anacrolix/torrent"
 )
 
 func main() {
 	err := mainErr()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error in main: %v", err)
+		log.Printf("error in main: %v", err)
 		os.Exit(1)
 	}
 }
@@ -27,14 +30,17 @@ func getInfohashes() (ret []torrent.InfoHash, err error) {
 		return nil, fmt.Errorf("opening db: %w", err)
 	}
 	defer conn.Close()
-	err = sqlitex.Exec(conn, "select distinct infohash from sample_infohashes_response_infohash where infohash not in (select infohash from info)", func(stmt *sqlite.Stmt) error {
-		var ih torrent.InfoHash
-		if read := stmt.ColumnBytes(0, ih[:]); read != 20 {
-			panic(read)
-		}
-		ret = append(ret, ih)
-		return nil
-	})
+	err = sqlitex.Exec(
+		conn,
+		"select distinct infohash from sample_infohashes_response_infohash where hex(infohash) not in (select upper(infohash) from info)",
+		func(stmt *sqlite.Stmt) error {
+			var ih torrent.InfoHash
+			if read := stmt.ColumnBytes(0, ih[:]); read != 20 {
+				panic(read)
+			}
+			ret = append(ret, ih)
+			return nil
+		})
 	if err != nil {
 		err = fmt.Errorf("selecting infohashes: %w", err)
 		return
@@ -48,6 +54,7 @@ func getDatabaseConn() (*sqlite.Conn, error) {
 
 func mainErr() error {
 	defer envpprof.Stop()
+	log.Default = log.Default.FilterLevel(log.Debug)
 	cl, err := torrent.NewClient(nil)
 	if err != nil {
 		return err
@@ -56,24 +63,38 @@ func mainErr() error {
 	http.HandleFunc("/torrentClientStatus", func(w http.ResponseWriter, r *http.Request) {
 		cl.WriteStatus(w)
 	})
+	dbConn, err := getDatabaseConn()
+	if err != nil {
+		return fmt.Errorf("getting database conn: %w", err)
+	}
+	defer dbConn.Close()
 	ihs, err := getInfohashes()
 	if err != nil {
 		return fmt.Errorf("getting infohashes: %w", err)
 	}
 	var wg sync.WaitGroup
+	log.Printf("got %v infohashes", len(ihs))
 	for _, ih := range ihs {
 		t, _ := cl.AddTorrentInfoHash(ih)
 		wg.Add(1)
 		go func(ih torrent.InfoHash) {
+			defer wg.Done()
 			defer t.Drop()
-			<-t.GotInfo()
-			conn, err := getDatabaseConn()
-			if err != nil {
-				log.Printf("error getting database conn: %v", err)
+			t.AddTrackers([][]string{{
+				"udp://tracker.coppersurfer.tk:6969/announce",
+				"http://tracker.opentrackr.org:1337/announce",
+			}})
+			started := time.Now()
+			select {
+			case <-t.GotInfo():
+			case <-time.After(5 * time.Minute):
+				log.Printf("timed out getting %v", ih)
 				return
 			}
-			defer conn.Close()
-			err = sqlitex.Exec(conn, "insert or ignore into info(infohash, bytes) values (?, ?)", nil, ih.HexString(), []byte(t.Metainfo().InfoBytes))
+			log.Printf("got info for %v (%v) after %v", ih, t, time.Since(started))
+			err = sqlitex.Exec(dbConn,
+				"insert or ignore into info(infohash, bytes) values (?, ?)", nil,
+				ih.HexString(), []byte(t.Metainfo().InfoBytes))
 			if err != nil {
 				log.Printf("error inserting info into db: %v", err)
 				return
@@ -81,6 +102,17 @@ func mainErr() error {
 			log.Printf("got info bytes for %v", ih)
 		}(ih)
 	}
-	wg.Wait()
-	return nil
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+	noInfohashes := make(chan struct{})
+	go func() {
+		wg.Wait()
+		close(noInfohashes)
+	}()
+	select {
+	case <-interrupt:
+		return errors.New("interrupted")
+	case <-noInfohashes:
+		return nil
+	}
 }
